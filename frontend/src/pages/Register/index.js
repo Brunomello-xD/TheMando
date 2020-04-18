5ffe2b0183abd58cb0bd71c6c51c445d39d9214d
Brunomello-xD <brunomello.TI@protonmail.com>
2020-04-17 21:31:01 -0300

    .

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,23 +1,25 @@
 import React from "react";
 
-import RegisterLogo from "../../assets/logoRegister.png";
 import lightSaber from "../../assets/light-saber.png";
+import lightSaber1 from "../../assets/light-saber-1.png";
 
 import "./styles.css";
 
 export default function Register() {
   return (
     <div className="registerContainer">
-      <img src={lightSaber} alt="Mando" />
+      <img src={lightSaber1} alt="Mando" />
       <section className="form">
-        <form>
-          <h1>Cadastro</h1>
-          <input placeholder="Nome" />
-          <input placeholder="Nome de usuário" />
-          <input type="email" placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
-          <button className="button">Tornar-se um Mandaloriano</button>
-        </form>
+        <div>
+          <form>
+            <h1>Cadastro</h1>
+            <input placeholder="Nome" />
+            <input placeholder="Nome de usuário" />
+            <input type="email" placeholder="E-mail" />
+            <input type="password" placeholder="Senha" />
+            <button className="button">Tornar-se um Mandaloriano</button>
+          </form>
+        </div>
       </section>
     </div>
   );
