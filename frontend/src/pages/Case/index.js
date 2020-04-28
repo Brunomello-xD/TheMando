20b7524bb015d0e5c4a7f876ac49b6cba9a4db40
Brunomello-xD <brunomello.TI@protonmail.com>
2020-04-28 20:20:29 -0300

    .

--- frontend/src/pages/Case/index.js
+++ frontend/src/pages/Case/index.js
@@ -1,23 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiMessageSquare } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 import logoMando from "../../assets/logoMain@2.png";
-import logoPerfilD from "../../assets/logoPerfilD.png";
+import imgPerfil from "../../assets/imgPerfil.png";
 
 import "./styles.css";
 
 export default function Case() {
   return (
     <div className="main">
-      <section className="menu">
-        <Link to="/cases">
-          <img src={logoMando} alt="Mando" />
-        </Link>
-      </section>
+      <div className="mainMenu">
+        <section className="contact">
+          <a href="https://github.com/Brunomello-xD" target="_blank">
+            <FaGithub className="github" size={12} color="#010601" />
+          </a>
+
+          <a href="https://github.com/Brunomello-xD" target="_blank">
+            <h1 className="github">GitHub</h1>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/bruno-mello-14058819b/"
+            target="_blank"
+          >
+            <FaLinkedin className="linkedin" size={12} color="#0c77b6" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/bruno-mello-14058819b/"
+            target="_blank"
+          >
+            <h1 className="linkedin">LinkedIn</h1>
+          </a>
+        </section>
+        <section className="menu">
+          <Link to="/cases">
+            <img src={logoMando} alt="Mando" />
+          </Link>
+          <Link className="perfil" to="/pefil">
+            <img src={imgPerfil} alt="ThePerfil" />
+          </Link>
+        </section>
+      </div>
 
       <section className="userTopic">
-        <img src={logoPerfilD} alt="TheMando" />
+        <img src={imgPerfil} alt="TheMando" />
         <h3>RestlinMagnat</h3>
       </section>
 
@@ -36,7 +64,7 @@ export default function Case() {
         <textarea cols="30" rows="10"></textarea>
 
         <button>
-          <FiMessageSquare size={18} />
+          <FiMessageSquare size={18} color="black" />
           Responder
         </button>
       </section>
